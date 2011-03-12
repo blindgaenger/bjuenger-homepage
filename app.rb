74348cb277947ead8e877cb794ccf180b2753458
blindgaenger <blindgaenger@gmail.com>
2011-03-12 20:01:40 +0100

    basic app setup like blindgaenger

---
+++ app.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'sass'
+require 'sinatra/content_for2'
+
+configure do
+  set :haml, :format => :html5 # default Haml format is :xhtml
+  set :sass, :style => :compact # default Sass style is :nested, :expanded
+end
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/:file.css' do
+  scss "stylesheets/#{params[:file]}".to_sym 
+end
