e1d98c5fd3e491974066a6366e25ead3f2386fc4
blindgaenger <blindgaenger@gmail.com>
2011-12-16 01:55:11 +0100

    use a page icon for the resume

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
 
   var iconMap = {
-    intro: 'home'
+    intro: 'user',
+    resume: 'page'
   }
-  
+
   $("nav a[href^='#']").anchorAnimate({speed : 700});
   $("#content section[id]").viewport(function() {
     var anchorId = $(this).attr('id');
     $('nav a').blur().selectCurrent('[href=#' + anchorId + ']');
-    
+
     var icon = iconMap[anchorId]
     var attr = {fill: "315-#333:15-#202020", stroke: "#333", "stroke-width": 2}
     $('#paper').drawIcon(icon, attr)
   });
-  
+
 });
