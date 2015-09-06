a506a01185f943a5da7b0f7247c4c2862c68b162
blindgaenger <blindgaenger@gmail.com>
2015-09-06 12:56:38 +0200

    new server command

--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-go run /Users/blindgaenger/Projects/Golang/src/github.com/blindgaenger/quodlibet/main/server.go --dir build
+go run /Users/blindgaenger/Projects/Golang/src/github.com/blindgaenger/quodlibet/main/server.go .
